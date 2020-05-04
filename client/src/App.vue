53e183784515b9df5b9dd63be52d8ca9272c513f
kensuke <sample@example.com>
2020-05-04 21:51:45 +0900

    [update] modify nav

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,8 +4,6 @@
       <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
       <el-menu-item index="1"><router-link to="/" style="text-decoration: none; display:block;">Principles for success 92, No.10</router-link></el-menu-item>
       <el-menu-item index="2"><router-link to="/about" style="text-decoration: none; display:block;">About</router-link></el-menu-item>
-      <!-- <el-menu-item index="1"><a href="/">Principles for success 92, No.10</a></el-menu-item>
-      <el-menu-item index="2"><a href="/about">About</a></el-menu-item> -->
     </el-menu>
 
       <h1 style="padding-top:4%;">Principles for success 92, No.10</h1>

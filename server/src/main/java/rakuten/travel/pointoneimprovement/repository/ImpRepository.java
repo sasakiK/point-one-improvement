e1383a7b5fb4b47d9abd22b69c03c09d100c119a
kensuke <sample@example.com>
2020-05-04 13:39:27 +0900

    [update] implemented more safe way for db insert

--- server/src/main/java/rakuten/travel/pointoneimprovement/repository/ImpRepository.java
+++ server/src/main/java/rakuten/travel/pointoneimprovement/repository/ImpRepository.java
@@ -2,10 +2,15 @@ package rakuten.travel.pointoneimprovement.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import rakuten.travel.pointoneimprovement.dto.Imp;
 import rakuten.travel.pointoneimprovement.dto.ImpRowMapper;
 
+import javax.swing.plaf.nimbus.State;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
@@ -28,11 +33,16 @@ public class ImpRepository {
     }
 
     public Integer saveImp(Imp imp) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
         LocalDateTime createdAt = LocalDateTime.now();
         String sql = "INSERT INTO improvement (content, created_at) VALUES (?, ?);";
-        jdbcTemplate.update(sql, imp.getContent(), createdAt.toString());
-        Integer id = jdbcTemplate.queryForObject("SELECT LAST_INSERT_ID()", Integer.class);
-        return id;
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, imp.getContent());
+            ps.setString(2, createdAt.toString());
+            return ps;
+            }, keyHolder);
+        return keyHolder.getKey().intValue();
     }
 
     public Integer updateImp(Imp imp, Integer id) {

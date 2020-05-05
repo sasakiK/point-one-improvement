0bc23ed3a5408ca528276c56c3b261e5f4f87746
kensuke <sample@example.com>
2020-05-05 22:31:55 +0900

    [update] remove h1 here

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,24 +1,17 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
-      <el-menu-item index="1"><router-link to="/" style="text-decoration: none; display:block;">Principles for success 92, No.10</router-link></el-menu-item>
-      <el-menu-item index="2"><router-link to="/about" style="text-decoration: none; display:block;">About</router-link></el-menu-item>
-    </el-menu>
-      <h1 style="padding-top:4%;">Principles for success 92, No.10</h1>
-    </div>
-    <router-view/>
-  </div>
+  <el-container id="app">
+    <el-header id="nav"><global-header/></el-header>
+    <el-main><router-view/></el-main>
+  </el-container>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        activeIndex: '1'
-      }
-    }
+import GlobalHeader from './components/GlobalHeader'
+export default {
+  components: {
+    GlobalHeader
   }
+}
 </script>
 
 <style lang="scss">
@@ -33,22 +26,5 @@
   width: 100%;
   height: 100%;
   overflow-x: hidden
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      // color: #42b983;
-      color: #C00014;
-    }
-  }
-  .el-menu--horizontal>.el-menu-item.is-active {
-    border-bottom: 2px solid #C00014;
-  }
 }
 </style>

37bcc977cccab5ecd6711955e94264a61e3eb01a
kensuke <sample@example.com>
2020-05-05 17:47:57 +0900

    [update] for using environment variable

--- client/src/main.js
+++ client/src/main.js
@@ -3,12 +3,24 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import ElementUI from 'element-ui' // 追記
-import locale from 'element-ui/lib/locale/lang/ja' // 追記
-import 'element-ui/lib/theme-chalk/index.css' // 追記
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/ja'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
-Vue.use(ElementUI, {locale}) // 追記
+Vue.use(ElementUI, {locale})
+
+var config = {
+  apiKey: process.env.VUE_APP_APIKEY,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,

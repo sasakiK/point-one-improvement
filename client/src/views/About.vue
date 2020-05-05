7daa30838bec1b86e56c606897d5530a8c0c4e3b
kensuke <sample@example.com>
2020-05-05 22:30:12 +0900

    [update] modify about

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="about">
-    <h4>Success comes from a series of 0.1% improvements, which will prepare you to seize great opportunities.</h4>
+  <div class="about" style="width:80%; margin:auto; text-align:left;">
+    <h3 style="padding-left: 5%; padding-top: 8%;">Success comes from a series of 0.1% improvements,</h3>
+    <h3 style="padding-left: 5%;">which will prepare you to seize great opportunities.</h3>
   </div>
 </template>
 
@@ -12,3 +13,14 @@
     methods:{}
   }
 </script>
+
+<style scoped>
+
+.one {
+  padding: ;
+  padding-left: 50px;
+}
+.h3 {
+  margin-left: 5%;
+}
+</style>
